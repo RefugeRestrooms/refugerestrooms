8e7b3e0b60fd17dc4d93fbe2f2ad7d3038ce044f
Teagan <tkwidmer@gmail.com>
2013-11-27 00:22:35 -0800

    initial commit. new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaferstallsRails::Application.config.secret_key_base = 'd1cbcde3c2974765f485a8189855c1d5776909a179b62870b219b3dc784137d7d18dadbe7206fbaecb3d9685265af7d04d5b3e6da27153f0c2d272f4cd6ca493'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaferstallsRails::Application.config.session_store :cookie_store, key: '_saferstalls_rails_session'
