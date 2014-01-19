a41851a9b150a3defdb1a1de7af41fb8ffddad07
Teagan <tkwidmer@gmail.com>
2014-01-19 14:17:30 -0800

    remove geocode and display lat  long

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -1,12 +1,4 @@
 class Bathroom < ActiveRecord::Base
-  attr_accessor :latitude, :longitude
-
-  geocoded_by :full_address
-  after_validation :geocode
-
-  def full_address
-    "#{street}, #{city}, #{state}"
-  end
 
 
 end
