8174233039f2f4be3f8289f5ba1e4adc6f91bd8c
Teagan <tkwidmer@gmail.com>
2014-02-06 17:50:40 -0800

    changing lookup back to google after doing client side geocoding of search

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -1,7 +1,7 @@
 class Bathroom < ActiveRecord::Base
 
   geocoded_by :full_address
-  after_validation :geocode
+  after_validation :geocode, :lookup => :google
 
   scope :unisex, -> (unisex) { where(bath_type: unisex) if unisex != nil }
   scope :ada, -> (ada) { where(access: ada) if ada != nil }
