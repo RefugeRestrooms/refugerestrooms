c0ddac3d8bdacbd776157e3b56dbcad6b4366f4a
Veronica <veronica.l.ray@gmail.com>
2014-02-07 23:38:03 -0800

    Set up strong params

--- app/admin/bathroom.rb
+++ app/admin/bathroom.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Bathroom do
 
+permit_params :name, :street, :city, :state, :access, :bath_type, :directions, :comment, :latitude, :longitude, :country
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
