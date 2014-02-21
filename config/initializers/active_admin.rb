2086571c2e28a61b14a9e179dc2026a83d3a4d65
Teagan <tkwidmer@gmail.com>
2014-02-20 18:15:30 -0800

    active admin tweak

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,12 +5,12 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Saferstalls Rails"
+  config.site_title = "Refuge Restrooms"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
