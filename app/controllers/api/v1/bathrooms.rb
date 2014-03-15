ea053fde4db36bc1dc4268a814ecdbe7aeabe710
Justin Bull <me@justinbull.ca>
2014-03-15 00:20:43 -0400

    Add support for filtering ADA and unisex bathrooms

--- app/controllers/api/v1/bathrooms.rb
+++ app/controllers/api/v1/bathrooms.rb
@@ -9,16 +9,30 @@ module API
 
       resource :bathrooms do
         desc "Get all bathroom records ordered by date descending."
+        params do
+          optional :ada, type: Boolean, desc: "Only return bathrooms that are ADA accessible."
+          optional :unisex, type: Boolean, desc: "Only return bathrooms that are unisex."
+        end
         get do
-          paginate(Bathroom.order(created_at: :desc))
+          b = Bathroom
+          b = b.where(access: 1) if params[:ada] === true
+          b = b.where(bath_type: 0) if params[:unisex] === true
+
+          paginate(b.order(created_at: :desc))
         end
 
         desc "Perform full-text search of bathroom records."
         params do
+          optional :ada, type: Boolean, desc: "Only return bathrooms that are ADA accessible."
+          optional :unisex, type: Boolean, desc: "Only return bathrooms that are unisex."
           requires :query, type: String, desc: "Your search query."
         end
         get :search do
-          paginate(Bathroom.text_search(params[:query]))
+          b = Bathroom
+          b = b.where(access: 1) if params[:ada] === true
+          b = b.where(bath_type: 0) if params[:unisex] === true
+
+          paginate(b.text_search(params[:query]))
         end
       end
     end
