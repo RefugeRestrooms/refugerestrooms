4b495bca50382e4c5c283e5cf52d0157dafa4b97
Buck Doyle <buck.doyle@gmail.com>
2014-04-05 16:00:24 -0700

    Use SimpleForm Bootstrap on submission form

---
+++ config/initializers/simple_form_bootstrap.rb
@@ -0,0 +1,87 @@
+# Taken from https://gist.github.com/adamico/6510093
+
+# http://stackoverflow.com/questions/14972253/simpleform-default-input-class
+# https://github.com/plataformatec/simple_form/issues/316
+
+inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  FileInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+]
+
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.push('form-control')
+    end
+  end
+
+  Object.const_set(input_type, new_class)
+end
+
+# Use this setup block to configure all options available in SimpleForm.
+SimpleForm.setup do |config|
+  config.boolean_style = :nested
+
+  config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: { input_html: { class: 'default_class' } } do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label_input
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+ config.wrappers :bootstrap3_horizontal, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '}, wrapper_html: { class: "col-lg-10 col-md-10"} } do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label
+    b.wrapper :right_column, tag: :div do |component|
+      component.use :input
+    end
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+  config.wrappers :group, tag: 'div', class: "form-group", error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '} }  do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label
+    b.use :input, wrap_with: { class: "input-group" }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+  config.default_wrapper = :bootstrap3
+end
