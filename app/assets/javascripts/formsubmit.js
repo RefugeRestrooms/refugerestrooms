355210dfab8171f99a9feeb8a6093b59c858a935
Buck Doyle <buck.doyle@gmail.com>
2014-04-07 11:00:48 -0700

    Remove logging statement of geocoding results

--- app/assets/javascripts/formsubmit.js
+++ app/assets/javascripts/formsubmit.js
@@ -13,7 +13,6 @@ $(function () {
       $('.currentLocationButton').removeClass('currentLocationButtonLocating');
       guessPosition(pos.coords, function (results) {
         if(results && results.length > 0){
-          console.log(results);
           var addressArray = results[0].address_components;
           $("#bathroom_street").val(addressArray[0].long_name + " " + addressArray[1].long_name);
           $("#bathroom_city").val(addressArray[3].long_name);
