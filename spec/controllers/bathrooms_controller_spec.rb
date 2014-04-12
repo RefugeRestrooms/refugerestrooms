a89c0c368284b04e93d55b8603507760925d66f8
Strand McCutchen <strand@bettermistak.es>
2014-04-12 14:48:31 -0700

    Test upvote and downvote actions in BathroomsController

--- spec/controllers/bathrooms_controller_spec.rb
+++ spec/controllers/bathrooms_controller_spec.rb
@@ -5,4 +5,20 @@ describe BathroomsController do
     get :index
     expect(response).to be_success
   end
+
+  context "voting" do
+    let(:bathroom) { FactoryGirl.create(:bathroom) }
+
+    it "should downvote" do
+      expect {
+        post :down_vote, id: bathroom.id
+      }.to change { bathroom.reload.downvote }.by 1
+    end
+
+    it "should upvote" do
+      expect {
+        post :up_vote, id: bathroom.id
+      }.to change { bathroom.reload.upvote }.by 1
+    end
+  end
 end
