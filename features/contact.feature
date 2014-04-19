791f75d5de28a695f90dd70a6248f16d1c50cced
Michael Wood <michael.wood@optimizely.com>
2014-04-18 22:21:18 -0700

    change test to only look for header

--- features/contact.feature
+++ features/contact.feature
@@ -2,7 +2,5 @@
 Feature: Request a contact form
 
   Scenario: Request from restroom
-    Given I submit a restroom in Vancouver
-    And I click to edit from restroom Vancouver restroom
-    Then I should be at the contact page with restroom_id and restoom_name queries
-    And I should see Vancouver restroom in the header
+    Given I click to edit from restroom Mission Creek Cafe restroom
+    And I should see Mission Creek Cafe restroom in the header
--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -1,16 +1,9 @@
-Given(/^I click to edit from restroom Vancouver restroom$/) do
+Given(/^I click to edit from restroom Mission Creek Cafe  restroom$/) do
   visit '/restrooms/1'
   click_link 'Contact us about this post!'
 end
 
-Then(/^I should be at the contact page with restroom_id and restoom_name queries$/) do
-  assert "#{uri.path}?#{uri.query}" == contact_path(:restroom_id => '1', :restroom_name => 'Vancouver restroom')
+Then(/^I should see Mission Creek Cafe  restroom in the header$/) do
+  expect(page).to have_content('Mission Creek Cafe  restroom')
 end
 
-Then(/^I should see Vancouver restroom in the header$/) do
-  expect(page).to have_content('Vancouver restroom')
-end
-
-def uri
-  URI.parse(current_url)
-end
