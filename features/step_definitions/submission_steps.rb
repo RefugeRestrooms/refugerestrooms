b0e310ca71a36f2da34c40268997d028a7a55606
Strand McCutchen <strand@bettermistak.es>
2014-08-18 00:12:39 -0400

    style fixes on submission steps feature test

--- features/step_definitions/submission_steps.rb
+++ features/step_definitions/submission_steps.rb
@@ -1,13 +1,13 @@
 Given(/^I submit a restroom in Vancouver$/) do
-  visit '/'
-  click_link 'Submit a New Restroom'
+  visit "/"
+  click_link "Submit a New Restroom"
 
-  fill_in 'restroom[name]', with: 'Vancouver restroom'
-  fill_in 'restroom[street]', with: '684 East Hastings'
-  fill_in 'restroom[city]', with: 'Vancouver'
-  fill_in 'restroom[state]', with: 'British Columbia'
-  find(:select, 'Country').first(:option, 'Canada').select_option
-  click_button 'Save Restroom'
+  fill_in "restroom[name]", with: "Vancouver restroom"
+  fill_in "restroom[street]", with: "684 East Hastings"
+  fill_in "restroom[city]", with: "Vancouver"
+  fill_in "restroom[state]", with: "British Columbia"
+  find(:select, "Country").first(:option, "Canada").select_option
+  click_button "Save Restroom"
 end
 
 Then(/^I should see that the restroom has been created$/) do
@@ -15,10 +15,10 @@ Then(/^I should see that the restroom has been created$/) do
 end
 
 When(/^I am in (.*) and I guess my location on the submission page$/) do |city|
-  visit '/'
-  click_link 'Submit a New Restroom'
+  visit "/"
+  click_link "Submit a New Restroom"
 
   mock_location(city.to_sym)
 
-  find('#guess').click
+  find("#guess").click
 end
