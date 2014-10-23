a55b1cc11cf68f9844bf5d8cd792d0889b86718b
Buck Doyle <buck.doyle@gmail.com>
2014-10-23 18:09:16 -0400

    Add default Akismet key for testing

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,2 +1,2 @@
-Rails.application.config.rakismet.key = ENV['AKISMET_KEY']
+Rails.application.config.rakismet.key = ENV['AKISMET_KEY'] || 'test'
 Rails.application.config.rakismet.url = 'http://refugerestrooms.org'
