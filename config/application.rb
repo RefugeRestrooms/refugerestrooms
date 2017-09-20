c446173c6ca4e906ec4597bb4e4aae2db78be645
DeeDeeG <DeeDeeG@users.noreply.github.com>
2017-09-20 14:49:40 -0400

    config: Merge config file changes from Rails 5.1.4
    Anything that would have changed functionality
    has been manually commented out.
    
    If we want to move to the new Rails 5.1 defaults,
    we are going to have to research what these new defaults do,
    and possibly switch to them and test that things work
    the way we want them to.
    
    This commit simply makes sure we have all the new config files,
    but as mentioned above, everything new is turned off for now.
    
    We may want to delete some of these (newly added) files
    if we aren't going to use them.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SaferstallsRails
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    # config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
