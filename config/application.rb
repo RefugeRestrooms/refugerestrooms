3c14d91a7a3933aaa404ad295c852c38e3759ca4
Jason Chen <kbtpodifo@gmail.com>
2018-01-10 18:21:11 -0500

    Correct indentation for config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module SaferstallsRails
         resource "/api/*", headers: :any, methods: [:get, :post, :options]
       end
     end
+
+    # I18n stuff
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
   end
 end
