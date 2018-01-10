bca00e5b3cf52063a63b51da383a71c3f516d3d6
Jason Chen <kbtpodifo@gmail.com>
2018-01-10 18:23:02 -0500

    Set website locale from Accept-Language header
    This is a good option because the header is usually set by the user's
    browser language preferences.
    
    Code from http://guides.rubyonrails.org/i18n.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :mobile_filter_header
+  before_action :set_locale
 
   def mobile_filter_header
     @mobile = true
   end
+
+  def mobile_filter_header
+    @mobile = true
+  end
+
+   def set_locale
+     I18n.locale = extract_locale_from_accept_language_header
+   end
+
+  private
+    def extract_locale_from_accept_language_header
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
 
 end
