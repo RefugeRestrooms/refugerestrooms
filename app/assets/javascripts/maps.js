469b29b7954314bd3826dc95d69d9cfb32dc4ed7
Jason Chen <kbtpodifo@gmail.com>
2018-01-10 21:54:37 -0500

    Replace all tabs with spaces in maps.js
    This is the standard approach and having mixed tabs and spaces isn't
    good.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,12 +1,12 @@
 var map;
 
 function initMap(x, y, image, draggable, callback){
-	image = typeof image !== 'undefined' ? image : currentLocationImage;
+  image = typeof image !== 'undefined' ? image : currentLocationImage;
 
-	// Draggable defaults to false
-	draggable =  draggable || false;
+  // Draggable defaults to false
+  draggable =  draggable || false;
 
-	//init map
+  //init map
   var mapOptions = {
     zoom: 15,
     center: new google.maps.LatLng(x, y)
@@ -23,32 +23,28 @@ function initMap(x, y, image, draggable, callback){
   var myLatLng = new google.maps.LatLng(x, y);
 
   var currentLocation = new google.maps.Marker({
-	  position: myLatLng,
-	  draggable: draggable,
-	  map: map,
-	  icon: image
+    position: myLatLng,
+    draggable: draggable,
+    map: map,
+    icon: image
   });
 
   currentLocation.addListener("dragend", callback);
 }
 
-
 function getPoint(string, callback){
-
-
-		//google Geocoding API
-		var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + string.replace(" ", "+");
-		$.get( url, function( data ) {
-			if(data && data.results){
-				if(data.results.length > 0){
-					callback(data.results[0].geometry.location);
-				}
-			}
-		});
+    // Google Geocoding API
+    var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + string.replace(" ", "+");
+    $.get( url, function( data ) {
+      if(data && data.results){
+        if(data.results.length > 0){
+          callback(data.results[0].geometry.location);
+        }
+      }
+    });
 }
 
 function placeMarker(lat, lng, content, number){
-
   // Create Icon
   var circleIcon = {
     url: circleMarkerImage,
@@ -81,16 +77,16 @@ function placeMarker(lat, lng, content, number){
 }
 
 function generateContent(data){
-	var item = $(".listItem[data-id=" + data.id + "]");
-	var container = $("<div class='markerPopup' />");
-	container.append(item.clone());
-	return container.html();
+  var item = $(".listItem[data-id=" + data.id + "]");
+  var container = $("<div class='markerPopup' />");
+  container.append(item.clone());
+  return container.html();
 }
 
 function setPoint(data, number){
-	if(data.latitude && data.longitude){
-		placeMarker(data.latitude, data.longitude, generateContent(data), number);
-	}
+  if(data.latitude && data.longitude){
+    placeMarker(data.latitude, data.longitude, generateContent(data), number);
+  }
 }
 
 /*
@@ -100,57 +96,57 @@ function setPoint(data, number){
 var currentLocationText = "Current Location";
 
 function searchLocation(search){
-	if (search == currentLocationText) {
-		searchCurrent();
-		return;
-	}
+  if (search == currentLocationText) {
+    searchCurrent();
+    return;
+  }
 
-	if(search == ""){
-		handleSearchResults(37.7577, -122.4376);
-		return;
-	}
+  if(search == ""){
+    handleSearchResults(37.7577, -122.4376);
+    return;
+  }
 
-	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({
-	  'address': search
-	}, function (results, status) {
-	  // if match
-	  if (status == google.maps.GeocoderStatus.OK) {
-			handleSearchResults(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-	  } else {
-		  emitError("Geocoder failed due to: " + status);
-	  }
-	});
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    'address': search
+  }, function (results, status) {
+    // if match
+    if (status == google.maps.GeocoderStatus.OK) {
+      handleSearchResults(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+    } else {
+      emitError("Geocoder failed due to: " + status);
+    }
+  });
 
 }
 
 function handleSearchResults(lat, lng){
-	$("#lat").val(lat);
-	$("#long").val(lng);
-	$(".search").find("form").submit();
+  $("#lat").val(lat);
+  $("#long").val(lng);
+  $(".search").find("form").submit();
 }
 
 function searchCurrent() {
-	var button = $('.currentLocationButton');
-	// if we are already searching, don't bother searching again
-	if (button.is('currentLocationButtonLocating')) {
-		return false;
-	}
+  var button = $('.currentLocationButton');
+  // if we are already searching, don't bother searching again
+  if (button.is('currentLocationButtonLocating')) {
+    return false;
+  }
 
-	button.addClass('currentLocationButtonLocating');
+  button.addClass('currentLocationButtonLocating');
 
-	// we get a promise back from this API
-	var location = locator.get();
+  // We get a promise back from this API
+  var location = locator.get();
 
-	// always turn off our "working..." class when we've found it
-	location.always(function() {
-		button.removeClass('currentLocationButtonLocating');
-	});
+  // always turn off our "working..." class when we've found it
+  location.always(function() {
+    button.removeClass('currentLocationButtonLocating');
+  });
 
-	location.then(function gotCoordinates(coords) {
-		$("#search").val(currentLocationText);
-		handleSearchResults(coords.latitude, coords.longitude);
-	}, emitError);
+  location.then(function gotCoordinates(coords) {
+    $("#search").val(currentLocationText);
+    handleSearchResults(coords.latitude, coords.longitude);
+  }, emitError);
 }
 
 /*
@@ -158,37 +154,36 @@ function searchCurrent() {
  */
 
 function emitError(error){
-	console.error(error.stack || error);
-	alert(error.message || error);
+  console.error(error.stack || error);
+  alert(error.message || error);
 }
 
 function guessPosition (coords, callback) {
-	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({
-	  'location': new google.maps.LatLng(coords.latitude, coords.longitude)
-	}, function (results, status) {
-	  // if match
-	  if (status == google.maps.GeocoderStatus.OK) {
-			callback(results);
-	  } else {
-		  emitError("Geocoder failed due to: " + status);
-	  }
-	});
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    'location': new google.maps.LatLng(coords.latitude, coords.longitude)
+  }, function (results, status) {
+    // if match
+    if (status == google.maps.GeocoderStatus.OK) {
+      callback(results);
+    } else {
+      emitError("Geocoder failed due to: " + status);
+    }
+  });
 }
 
 $(function(){
-	var $map = $('#mapArea');
-	if($map.length > 0) {
-		initMap($map.data('latitude'), $map.data('longitude'), showMarkerImage);
-	}
+  var $map = $('#mapArea');
+  if($map.length > 0) {
+    initMap($map.data('latitude'), $map.data('longitude'), showMarkerImage);
+  }
 
-	window.Maps = {
-		reloadMap: function(map) {
-			initMap(map.dataset.latitude, map.dataset.longitude, showMarkerImage);
-		},
-
-		reloadDraggable: function(map, callback) {
-		  initMap(map.dataset.latitude, map.dataset.longitude, showMarkerImage, true, callback);
-		}
-	}
+  window.Maps = {
+    reloadMap: function(map) {
+      initMap(map.dataset.latitude, map.dataset.longitude, showMarkerImage);
+    },
+    reloadDraggable: function(map, callback) {
+      initMap(map.dataset.latitude, map.dataset.longitude, showMarkerImage, true, callback);
+    }
+  }
 });
