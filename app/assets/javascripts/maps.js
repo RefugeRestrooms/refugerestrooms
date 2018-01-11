3f7f316d8f9437c496491f9f8dc4d48ad8443901
Jason Chen <kbtpodifo@gmail.com>
2018-01-10 21:07:23 -0500

    Use google maps built-in custom marker
    Using the built-in marker eliminates the need for the NumberedCircle
    class and significantly simplifies the code, since now the Google
    API takes care of positioning the marker, the label, and the
    info window.
    
    This also solves the overflow and lack of padding problems that the
    old info window had.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -49,141 +49,48 @@ function getPoint(string, callback){
 
 function placeMarker(lat, lng, content, number){
 
-	//create marker
-	var marker = new NumberedCircle(new google.maps.LatLng(lat,lng), circleMarkerImage, number, content);
+  // Create Icon
+  var circleIcon = {
+    url: circleMarkerImage,
+    size: new google.maps.Size(37, 37),
+    anchor: new google.maps.Point(18.5, 18.5)
+  };
 
+  // Create label
+  var markerLabel = {
+    color: 'white',
+    text: number.toString()
+  };
+
+  // Create marker
+  var marker = new google.maps.Marker({
+    position: {lat: lat, lng: lng},
+    icon: circleIcon,
+    label: markerLabel,
+    map: map
+  });
+
+  // Create info window
+  var infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+
+  marker.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });
 }
 
 function generateContent(data){
 	var item = $(".listItem[data-id=" + data.id + "]");
 	var container = $("<div class='markerPopup' />");
 	container.append(item.clone());
-
-	return container;
+	return container.html();
 }
 
 function setPoint(data, number){
 	if(data.latitude && data.longitude){
 		placeMarker(data.latitude, data.longitude, generateContent(data), number);
 	}
-}
-
-
-/*
-* Create a new marker that allows text
-*/
-NumberedCircle.prototype = new google.maps.OverlayView();
-
-function NumberedCircle(center, image, text, content) {
-	// Initialize all properties.
-	this.center_ = center;
-	this.image_ = image;
-	this.text_ = text;
-	this.map_ = map;
-	this.content_ = content;
-
-	// this will hold the div displayed on the map
-	this.div_ = null;
-
-	// Place on map
-	this.setMap(map);
-}
-
-NumberedCircle.prototype.onAdd = function() {
-	var me = this;
-
-	//generate the content
-	//TODO: move all style to css using className
-	var div = document.createElement('div');
-	div.style.borderStyle = 'none';
-	div.style.borderWidth = '0px';
-	div.style.position = 'absolute';
-	div.className = "numberCircle";
-	div.onclick = function(){
-		me.openPopup(me.content_);
-		};
-
-	var img = document.createElement('img');
-	img.className = "mapIconImage";
-	img.src = this.image_;
-	img.style.position = 'absolute';
-	div.appendChild(img);
-
-	var textDiv = document.createElement('div');
-	textDiv.className = "numberCircleText";
-	textDiv.style.position = 'absolute';
-	textDiv.style.top = 0;
-	textDiv.style.left = 0;
-	textDiv.style.width = '100%';
-	textDiv.style.height = '100%';
-	textDiv.innerHTML = this.text_;
-	div.appendChild(textDiv);
-
-	this.content_.appendTo(div);
-
-	this.div_ = div;
-
-	// Add the element to the "overlayLayer" pane.
-	var panes = this.getPanes();
-	panes.overlayMouseTarget.appendChild(div);
-};
-
-NumberedCircle.prototype.draw = function() {
-
-	// retrieve the projection from the overlay.
-	var overlayProjection = this.getProjection();
-
-	// convert the lat/lng to pixel units
-	var center = overlayProjection.fromLatLngToDivPixel(this.center_);
-
-	//get image for size calculations
-	var div = this.div_;
-	var image = div.getElementsByTagName("IMG")[0];
-
-	//detect if image has loaded yet or not (we can't get the size until it's loaded)
-	if(image.width == 0){
-		//set div's location, only thing we can do until image loads
-		div.style.left = center.x + 'px';
-		div.style.top = center.y + 'px';
-
-		image.onload = function(){
-			var parent = this.parentNode;
-			//offset the parent div so that the image is centered
-			parent.style.left = (parent.offsetLeft - (this.width / 2)) + 'px';
-			parent.style.top = (parent.offsetTop - (this.height / 2)) + 'px';
-			parent.style.width = this.width + 'px';
-			parent.style.height = this.height + 'px';
-		}
-	}else{
-		//the image is loaded so we can easily access the size
-		//offset the div so that the image is centered
-		if(map.getZoom() <= 14){
-			image.width = image.naturalWidth / 2;
-		}else{
-			image.width = image.naturalWidth;
-		}
-		div.style.left = (center.x - (image.width / 2)) + 'px';
-		div.style.top = (center.y - (image.height / 2)) + 'px';
-		div.style.width = image.width + 'px';
-		div.style.height = image.height + 'px';
-
-		//adjust line-height to center text
-		div.children[1].style.lineHeight = image.height + 'px';
-	}
-};
-
-NumberedCircle.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
-};
-
-NumberedCircle.prototype.openPopup = function(content){
-	$(content).show();
-
-	//hide popup after 10 seconds
-	setTimeout(function(){
-		content.fadeOut();
-		}, 10000);
 }
 
 /*
