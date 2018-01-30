c2db35f414fcc9004d44dcfcc42d8ff1ef344e9f
Jason Chen <kbtpodifo@gmail.com>
2018-01-29 22:45:45 -0500

    Small style changes
    Changes:
    - Rename functions, variables
    - Have comments start with spaces

--- app/assets/javascripts/views/restrooms/index.js
+++ app/assets/javascripts/views/restrooms/index.js
@@ -1,8 +1,7 @@
 $(function(){
   var headerHidden = false;
-  var mapOn = false;
+  var mapInitialized = false;
   var mapShow = false;
-  var mapHeight, listHeight;
   var mapContainer = $("#mapContainer");
   var list = $("#list");
   var mapToggle = $(".mapToggle");
@@ -21,38 +20,36 @@ $(function(){
     mapShow = !mapShow;
   }
 
-  function doInit(){
-    //initialize the map if it wasn't already on
-    if (!mapOn && mapContainer.data('latitude') && mapContainer.data('longitude')) {
+  function initPoints(){
+    // initialize the map if it wasn't already on
+    if (!mapInitialized && mapContainer.data('latitude') && mapContainer.data('longitude')) {
       initMap(mapContainer.data('latitude'), mapContainer.data('longitude'));
 
-      //get a list of points from the server based on the searched location
+      // get a list of points from the server based on the searched location
       $.get( '/restrooms' + window.location.search , {}, function( data ) {
         for(var i = 0; i < data.length; i++){
-        //for each point in the data, put a point on the map
+        // for each point in the data, put a point on the map
         setPoint(data[i], i + 1);
         }
       }, 'json');
-      mapOn = true;
+      mapInitialized = true;
     }
   }
 
 
   if (mapContainer.length > 0 && list.length > 0 && mapToggle.length > 0) {
     if (!mapContainer.data('latitude') || !mapContainer.data('longitude')) {
-      //catch bad URL
+      // catch bad URL
       searchLocation(search.val());
     }
-    //get default height for animation later
+    // get default height for animation later
     mapHeight = mapContainer.height();
     listHeight = list.height();
 
     // hide the map
     mapContainer.fadeOut(0);
 
-    mapToggle.click(function(){
-      //toggle which display is open
-      toggleMap();
-    });
+    // toggle which display is open
+    mapToggle.click(toggleMap);
   }
 });
