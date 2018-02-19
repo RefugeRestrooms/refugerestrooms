3d575526d717873df09b7dfc050537b5eec4f651
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-02-18 19:32:43 -0500

    poltergeist: Increase timeout time

--- features/support/poltergeist.rb
+++ features/support/poltergeist.rb
@@ -1,7 +1,7 @@
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist_debug do |app|
-  Capybara::Poltergeist::Driver.new(app, :inspector => true)
+  Capybara::Poltergeist::Driver.new(app, timeout: 60, :inspector => true)
 end
 
 Capybara.javascript_driver = :poltergeist_debug
