30faee5ca0f75e1af34a6d735ce8e9a35624c22e
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-09-30 16:24:19 -0400

    application_controller.rb: Adjust set_locale logic
    Adds a :locale parameter (which should come from the URL, e.g.:
    `refugerestrooms.org/?locale=en` or something like that.)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
    def set_locale
-     I18n.locale = http_accept_language.language_region_compatible_from(I18n.available_locales)
+     I18n.locale = params[:locale] || http_accept_language.language_region_compatible_from(I18n.available_locales)
    end
 
 end
