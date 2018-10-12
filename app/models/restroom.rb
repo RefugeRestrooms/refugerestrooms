bccf8ee5589163cb62f6ec033e7b08ba377bf338
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-10-12 11:50:21 -0400

    Docker: Don't geocode the db seed entries
    They are already geocoded and do not need to be sent to the Maps API.
    
    This saves quota usage, and prevents error messages when we are over quota limit.

--- app/models/restroom.rb
+++ app/models/restroom.rb
@@ -79,6 +79,7 @@ class Restroom < ApplicationRecord
 
     def perform_geocoding
       return true if Rails.env == "test"
+      return true if ENV["SEEDING_DONT_GEOCODE"]
       geocode
     end
 end
