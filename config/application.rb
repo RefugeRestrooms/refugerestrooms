343ec5552d3d82e6ec0a3e23748494504f2b5f8a
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-10-29 15:47:24 -0400

    config/application.rb: Add Italian locale
    Enables the app to load Italian translations.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module SaferstallsRails
 
     # I18n stuff
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.available_locales = [:en, :es, :fr, :pl]
+    config.i18n.available_locales = [:en, :es, :fr, :it, :pl]
     #config.i18n.default_locale = :fr
 
 
