810ac0f425fb4807d1c075b3630a16d8f30dd773
Morgan Fogarty <mofo37@gmail.com>
2019-03-30 16:26:00 -0700

    Add ability to upload CSV

--- app/controllers/bulk_uploads_controller.rb
+++ app/controllers/bulk_uploads_controller.rb
@@ -1,4 +1,23 @@
 class BulkUploadsController < ApplicationController
   def new
+    @bulk_upload = BulkUpload.new
   end
+
+  def create
+    @bulk_upload = BulkUpload.new(bulk_upload_params)
+    if @bulk_upload.save
+      redirect_to bulk_upload_path(@bulk_upload)
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    
+  end
+ 
+  private
+    def bulk_upload_params
+      params.require(:bulk_upload).permit(:file)
+    end
 end
