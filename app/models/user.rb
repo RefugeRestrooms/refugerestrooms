cd1d287be66b9afc573f1d51a9d137cf900f912a
Morgan Fogarty <mofo37@gmail.com>
2019-03-31 12:31:44 -0700

    Add association between bulk_upload and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bulk_uploads
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_174147) do
+ActiveRecord::Schema.define(version: 2019_03_31_182012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2019_03_31_174147) do
   create_table "bulk_uploads", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bulk_uploads_on_user_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
