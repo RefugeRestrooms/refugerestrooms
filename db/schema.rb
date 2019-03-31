fd2ef216aa600e2f06af86340b0c6338aadf02ee
Lisa Vogt <lisavogt.sf@gmail.com>
2019-03-31 10:48:08 -0700

    add Approved column to devise-generated user table
    Co-authored-by: Chelsea Troy <chelsea.dommert@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_233323) do
+ActiveRecord::Schema.define(version: 2019_03_31_174147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 2019_03_30_233323) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
