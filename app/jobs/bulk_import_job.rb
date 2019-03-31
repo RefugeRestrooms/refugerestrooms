190026d85c6faa55291e0f40d9ae7621be26d78d
Caleb Harris <caleb.harris@mac.com>
2019-03-31 15:06:42 -0700

    First pass at bulk import and geocoding job

--- app/jobs/bulk_import_job.rb
+++ app/jobs/bulk_import_job.rb
@@ -1,8 +1,27 @@
+require 'csv'
+require 'geocoder'
+
 class BulkImportJob < ApplicationJob
   queue_as :default
 
-  def perform(bulk_job)
-    Rails.logger.info bulk_job.class
-    Rails.logger.info bulk_job
+  def perform(bulk_upload)
+    contents = bulk_upload.file.download
+    Rails.logger.info Geocoder.config
+
+    CSV.parse(contents, headers: true) do |row|
+      values = row.to_hash.slice "name", "street", "city", "state", "country"
+      rr = Restroom.new values
+      if rr.valid?
+        if rr.latitude && rr.longitude
+          rr.save
+        else
+          # TODO: add row_error
+          Rails.logger.warn "Unable to geocode data: #{values}"
+        end
+      else
+        # TODO: also add row_error
+        Rails.logger.warn "Unable to validate data: #{values}"
+      end
+    end
   end
 end
