2d64fd070d1b2c8051c6e6aedf45ce643dabff44
Betsy Haibel <betsy.haibel@gmail.com>
2019-04-01 13:59:04 -0400

    hacky error handling for imports

--- app/jobs/bulk_import_job.rb
+++ app/jobs/bulk_import_job.rb
@@ -7,6 +7,7 @@ class BulkImportJob < ApplicationJob
   def perform(bulk_upload)
     contents = bulk_upload.file.download
     Rails.logger.info Geocoder.config
+    row_level_errors = []
 
     CSV.parse(contents, headers: true) do |row|
       values = row.to_hash.slice "name", "street", "city", "state", "country"
@@ -16,13 +17,34 @@ class BulkImportJob < ApplicationJob
         if rr.latitude && rr.longitude
           SaveRestroom.new(rr, false).call
         else
-          # TODO: add row_error
+          row_level_errors << NonGeocodableRow.new(values)
           Rails.logger.warn "Unable to geocode data: #{values}"
         end
       else
-        # TODO: also add row_error
+        row_level_errors << InvalidRow.new(values)
         Rails.logger.warn "Unable to validate data: #{values}"
       end
     end
+
+  # This is a stupidest-thing-that-could-work solution
+  # due to limited time at a hackathon.
+  # Please replace me with real validation error handling in the future.
+  if row_level_errors.length > 0
+    raise "problems with at least one CSV row"
   end
+
+  rescue e
+    raise GenericError.new(message: e.message, upload: bulk_upload, row_level_errors: row_level_errors)
+  end
+
+  class GenericError < StandardError
+    def initialize(message:, upload:, row_level_errors:)
+      @upload = upload
+      @row_level_errors = row_level_errors
+      super(message)
+    end
+  end
+
+  NonGeocodableRow = Struct.new(:row)
+  InvalidRow = Struct.new(:row)
 end
--- spec/jobs/bulk_import_job_spec.rb
+++ spec/jobs/bulk_import_job_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe BulkImportJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
