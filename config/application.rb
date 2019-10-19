9b76d0591e94719c267b8c0f132d019a142dabd3
DeeDeeG <DeeDeeG@users.noreply.github.com>
2019-10-19 14:09:54 -0400

    config/application.rb: Set default locale to "en"

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module SaferstallsRails
     # I18n stuff
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :es, :fil, :fr, :hi, :it, :pl, :"pt-BR"]
-    #config.i18n.default_locale = :fr
+    config.i18n.default_locale = :en
 
 
 
