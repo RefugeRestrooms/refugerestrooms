d1d9c1c73d756829207bbd41eaaa2cb33abd3211
Emily Ring <emily_ring@ymail.com>
2019-12-16 06:43:17 -0500

    Added rails-erb-loader to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,16 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
 
+// jQuery
+const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
   })
 )
+
+// rails-erb-loader
+const erb =  require('./loaders/erb')
+environment.loaders.append('erb', erb)
 
 module.exports = environment
