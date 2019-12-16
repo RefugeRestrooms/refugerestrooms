780b0cf2759fa8471ed87fd37b91b0ec22550ace
Emily Ring <emily_ring@ymail.com>
2019-12-16 06:25:41 -0500

    Ran webpack:install process.  Working without rails-erb-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,11 @@
 const { environment } = require('@rails/webpacker')
-const erb =  require('./loaders/erb')
-const webpack = require("webpack")
+const webpack = require('webpack')
 
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default'],
-}))
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
-environment.loaders.append('erb', erb)
 module.exports = environment
