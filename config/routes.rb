85f19985e92573d433fd061550266ff01706d76c
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-04-10 12:41:19 -0400

    config/routes.rb: Add (optional) locale scope to all pages
    Adds an optional locale prefix for almost every page in the app.
    
    See: https://guides.rubyonrails.org/v5.2.4/i18n.html#setting-the-locale-from-url-params
    
    You can now visit e.g. '/es/restrooms/new' and you will see the
    "Submit a New Restroom" page in Spanish.
    
    All links in the app auto-generate this locale prefix,
    so users can specify a locale via visiting a specific locale prefix,
    and the links in the app will not misdirect them into another locale.
    
    (You can still visit URLs without a locale prefix,
    like '/restrooms/new', and the app will simply auto-detect the locale
    based on the preferred languages in your browser settings.)
    
    This preserves the existing URLs in working order,
    and any links out there on the web, or in people's bookmarks,
    will still work.)
    
    The home page does not always get the prefix:
    - The home page link in nav is just '/?locale=[I18n.locale]'
      - You can visit '/?locale=es' to see the homepage in Spanish.
      - The homepage link in the nav are auto-generated
        as '/?locale=[I18n.locale]'; This is equivalent in functionality
        to the locale prefixes (e.g. '/es/').
      - The homepage links in the footer are auto-generated as
        '/[I18n.locale]/'.
      - You can visit '/es/' to see the homepage in Spanish.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,27 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+    get '/:locale' => 'pages#index'
+    root 'pages#index'
+
     ActiveAdmin.routes(self)
-    resources :restrooms, except: [:edit, :destroy]
+    scope "(:locale)" do
+      resources :restrooms, except: [:edit, :destroy]
+    end
 
     namespace :api do
-      resources :docs, only: [:index]
+      scope "(:locale)" do
+        resources :docs, only: [:index]
+      end
     end
 
     mount API::Base => '/api'
+    scope "(:locale)" do
+      get '/contact', to: 'contacts#new'
+      get "/*id" => 'pages#show', as: :page, format: false
+    end
 
-    get '/contact', to: 'contacts#new'
-    get "/*id" => 'pages#show', as: :page, format: false
-    root 'pages#index'
-
-    resources "contacts", only: [:new, :create]
+    scope "(:locale)" do
+      resources "contacts", only: [:new, :create]
+    end
 end
