9f0c354fe0f5f2ff005a6053a4750fd69814d681
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-04-14 12:43:17 -0400

    spec: Fix a path construction, test passes now
    Passing the whole array of restroom data was causing the whole
    array to be erroneously interpreted as if it was the :locale
    prefix in the path.
    
    Explicitly pass :id, and only :id,
    to the `restroom_path` route helper for this test.
    
    (We only need the :id from the newly-constructed test restroom
    in order to visit the correct path. Other restroom data isn't
    needed here anyhow.)

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -4,7 +4,7 @@ describe 'the contact process', type: :feature do
   it 'should show a generic contact when contact is not from restroom form' do
     restroom = create(:restroom, name: "Mission Creek Cafe")
 
-    visit restroom_path restroom
+    visit restroom_path(:id => restroom.id)
     click_link 'Contact'
 
     expect(page).to_not have_content('Mission Creek Cafe')
