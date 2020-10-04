4623f16c80d116bda112608f4a8dbea3b9a705cb
Giorgio Torres <torres.giorgio@gmail.com>
2020-10-03 22:17:05 -0300

    Keeping filters state on pagination

--- app/javascript/packs/views/restrooms/restrooms.js
+++ app/javascript/packs/views/restrooms/restrooms.js
@@ -3,27 +3,62 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(() => {
+  function getSearchParams() {
+    const { search: search } = location;
+    return new URLSearchParams(search);
+  }
+  function applyFilters(filters) {
+    const { origin: url, pathname: path } = location;
+    const searchParams = getSearchParams();
+    searchParams.set('filters', filters);
+    location = `${url}${path}?${searchParams.toString()}`
+  }
+
+  function getFilters() {
+    const searchParams = getSearchParams();
+    const filters = searchParams.get('filters') || '';
+    return filters.split(',').filter(filter => filter && filter.length > 0);
+  }
+
+  function removeFilter(toRemove) {
+    const filters =
+      getFilters()
+      .filter((filter) => filter != toRemove);
+
+    applyFilters(filters);
+  }
+
+  function addFilter(filter) {
+    let filters = getFilters();
+
+    if (filters.indexOf(filter) == -1) {
+      filters.push(filter);
+    }
+
+    applyFilters(filters);
+  }
+
   $('#ada_filter').click(function() {
     if ($(this).hasClass("active")) {
-      $('.listItem.not_accessible').show();
+      removeFilter('accessible');
     } else {
-      $('.listItem.not_accessible').hide();
+      addFilter('accessible');
     }
   });
 
   $('#unisex_filter').click(function() {
     if ($(this).hasClass("active")) {
-      $('.listItem.not_unisex').show();
+      removeFilter('unisex');
     } else {
-      $('.listItem.not_unisex').hide();
+      addFilter('unisex');
     }
   });
 
   $('#changing_table_filter').click(function() {
     if ($(this).hasClass("active")) {
-      $('.listItem.no_changing_table').show();
+      removeFilter('changing_table');
     } else {
-      $('.listItem.no_changing_table').hide();
+      addFilter('changing_table');
     }
   });
 });
