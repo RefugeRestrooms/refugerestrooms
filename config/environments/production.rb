3446a4154a41a0aabf6a2d69d081d8e703479b8d
Isaac Hudson <ice1080@gmail.com>
2023-12-11 17:13:02 -0800

    fixing configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = {:tl => [:fil,I18n.default_locale]}
+  config.i18n.fallbacks.default = I18n.default_locale
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
