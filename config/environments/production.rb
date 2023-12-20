f465bd46c1d900cd9e642954fd07cd384b0289f0
Isaac Hudson <ice1080@gmail.com>
2023-12-19 19:22:54 -0800

    attempting to fix production not loading api docs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Need autoloading to work in production too
+  Rails.application.config.enable_dependency_loading
 end
